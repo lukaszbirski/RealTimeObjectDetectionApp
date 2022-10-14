26eadb9d376aaddc716ccbfdeb26d445e168cf94
Lukasz <lukasz.birski@gmail.com>
2022-10-14 22:39:03 +0200

    Added camera permission

--- app/src/main/java/pl/birski/realtimeobjectdetectionapp/MainActivity.kt
+++ app/src/main/java/pl/birski/realtimeobjectdetectionapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package pl.birski.realtimeobjectdetectionapp
 
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import pl.birski.realtimeobjectdetectionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var requestSinglePermission = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) {
+        it.entries.forEachIndexed() { index, _ ->
+            PermissionUtil.returnPermissionsArray()[index]
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        askForPermissions()
+
+        setContentView(binding.root)
+    }
+
+    private fun askForPermissions() {
+        requestSinglePermission.launch(
+            PermissionUtil.returnPermissionsArray()
+        )
     }
 }
---
+++ app/src/main/java/pl/birski/realtimeobjectdetectionapp/PermissionUtil.kt
@@ -0,0 +1,16 @@
+package pl.birski.realtimeobjectdetectionapp
+
+import android.Manifest
+import android.content.Context
+import pub.devrel.easypermissions.EasyPermissions
+
+object PermissionUtil {
+    fun hasMessagesPermission(context: Context) = EasyPermissions.hasPermissions(
+        context,
+        Manifest.permission.CAMERA
+    )
+
+    fun returnPermissionsArray() = arrayOf(
+        Manifest.permission.CAMERA
+    )
+}
