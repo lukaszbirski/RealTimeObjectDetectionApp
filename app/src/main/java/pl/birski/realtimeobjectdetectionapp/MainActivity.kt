4552e3dd3cb67f230457fa1a5a3bbe0e1e9e3060
Lukasz <lukasz.birski@gmail.com>
2022-10-14 21:13:21 +0200

    Initial commit

---
+++ app/src/main/java/pl/birski/realtimeobjectdetectionapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.birski.realtimeobjectdetectionapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
